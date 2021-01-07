aaa49079062b50945252f6c4a18f5161429f56c8
lele <ye2000104@gmail.com>
2021-01-07 22:57:40 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button type="primary" @click="logout">退出</el-button>
+  </div>
+</template>
+<script>
+export default {
+  methods:{
+    logout(){
+      // 清除token
+      window.sessionStorage.clear()
+      // 编程时导航会登录页面
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 引入全局样式
+import './assets/css/gloabl.css'
+// 引入字体图标
+import './assets/fonts/iconfont.css'
+// 对axios进行全局配置
+import axios from 'axios'
+// 为axios设置根路径
+axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+// 把axios挂载到Vue的原型链上
+Vue.prototype.$http = axios
+
+
 
 Vue.config.productionTip = false
 
